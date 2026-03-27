549b9b2ed2b0b4cdbc52ab63b1f1926a01379ce4
Katherine <kalchenko200345@gmail.com>
2026-03-27 17:16:55 +0300

    changed routes

--- app/api/task/routes.py
+++ app/api/task/routes.py
@@ -3,5 +3,5 @@ from .views import TaskOne, TaskStatus, TaskRegenerate
 
 #для каждого вью класса регистрируем url
 task.add_url_rule('/first', view_func=TaskOne.as_view('first'))
-task.add_url_rule('/first', view_func=TaskStatus.as_view('status'))
-task.add_url_rule('/first', view_func=TaskRegenerate.as_view('regenerate'))
+task.add_url_rule('/<uuid:task_id>/status', view_func=TaskStatus.as_view('status'))
+task.add_url_rule('/<uuid:task_id>/regenerate', view_func=TaskRegenerate.as_view('regenerate'))
