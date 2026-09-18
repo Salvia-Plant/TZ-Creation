51d4081f1b6bbe716910a7cc4b111928fbd7053f
Katherine <kalchenko200345@gmail.com>
2026-09-18 17:43:05 +0300

    точно последнее изменение вношу косметические правки

--- app/api/task/routes.py
+++ app/api/task/routes.py
@@ -1,5 +1,5 @@
 from . import task
-from .views import TaskList, SingleTask, TaskStatus, TaskRegenerate, Statuses,TaskUpdate, AdmittedPeople,\
+from .views import TaskList, SingleTask, TaskStatus, Statuses,TaskUpdate, AdmittedPeople,\
 Autogenerate
 
 #для каждого вью класса регистрируем url
@@ -8,7 +8,7 @@ task.add_url_rule('/<uuid:task_id>/status', view_func=TaskStatus.as_view('status
 task.add_url_rule('/<uuid:task_id>/task_update', view_func=TaskUpdate.as_view('task_update'))
 task.add_url_rule('/statuses', view_func=Statuses.as_view('statuses'))
 task.add_url_rule('/<uuid:task_id>/single_task', view_func=SingleTask.as_view('single_task'))
-task.add_url_rule('/<uuid:task_id>/regenerate', view_func=TaskRegenerate.as_view('regenerate'))
+#task.add_url_rule('/<uuid:task_id>/regenerate', view_func=TaskRegenerate.as_view('regenerate'))
 task.add_url_rule('/<uuid:task_id>/people', view_func=AdmittedPeople.as_view('admitted_people'))
 task.add_url_rule('/<uuid:task_id>/autogenerate', view_func=Autogenerate.as_view('autogenerate'))
 
